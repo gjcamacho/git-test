058c27f65c3677a5dc2ace3ac18ff48a72103772
Gerardo Camacho <gjcamacho@hotmail.com>
2015-11-04 17:18:44 -0600

    initialization of the repo

---
+++ src/GitTestMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author gjcamacho
+ *
+ */
+public class GitTestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
